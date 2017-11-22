94566e450e706c3963d5f2d0302067698064e3c7
xieqixiang <xie_514328234@163.com>
2017-11-22 16:48:18 +0800

    add
    committ

---
+++ Assets/_Scripts/FollowCam.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool ___________;
+
+    public GameObject poi;
+
+    public float camZ;
+
+    void Awake() {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 destination;
+
+
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = poi.transform.position;
+            if (poi.tag == "Projectile") {
+                if (poi.GetComponent<Rigidbody>().IsSleeping()) {
+                    poi = null;
+                    return;
+                }
+            }
+        }
+
+        
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination.z = camZ;
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+
+        transform.position = destination;
+
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10; 
+     }
+
+}
---
+++ Assets/_Scripts/Goal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+
+    static public bool goalMet = false;
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.tag == "Projectile") {
+            Goal.goalMet = true;
+
+            Color c =this.GetComponent<Renderer>().material.color;
+
+            c.a = 1;
+
+            this.GetComponent<Renderer>().material.color = c;
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
